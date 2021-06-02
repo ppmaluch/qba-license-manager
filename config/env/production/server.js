a91352e3316e5d48d56c850b482d846f9bebd8bc
ppmaluch <ppmaluch@gmail.com>
2021-06-02 13:34:58 -0400

    modified env vars for production

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,12 @@
 module.exports = ({ env }) => ({
-  url: env("URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "b79cb80a59bb288aca68cfaa805f9406"),
+    },
+  },
+  cron: {
+    enabled: env.bool("CRON_ENABLED", false),
+  },
 });
