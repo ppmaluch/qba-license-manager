d335794753e6dfaeedcb17e9430ecdf0c5f602cf
ppmaluch <ppmaluch@gmail.com>
2021-06-02 13:47:12 -0400

    modify database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -13,13 +13,9 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      options: {
         ssl: false,
       },
+      options: {},
     },
   },
 });
