3e6cb2e481f9c19819589accc16163d2af242830
ppmaluch <ppmaluch@gmail.com>
2021-06-02 14:16:19 -0400

    modified server conf

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("URL"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "b79cb80a59bb288aca68cfaa805f9406"),
