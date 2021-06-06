7abe3a894c8c0ab71e3dc96808d8db00ea535597
ppmaluch <ppmaluch@gmail.com>
2021-06-06 19:00:30 -0400

    [add] logic to search an app by user-friendly url

--- api/app/controllers/app.js
+++ api/app/controllers/app.js
@@ -1,8 +1,15 @@
 'use strict';
+const {sanitizeEntity} = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async findOneUrl(ctx){
+        const {url} = ctx.params;
+        const entity = await strapi.services.app.findOne({url});
+        return sanitizeEntity(entity, {model: strapi.models.app});
+    }
+};
