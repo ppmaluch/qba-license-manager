00646451ad3416b74bdbce5c9c58a319ba2233b9
ppmaluch <ppmaluch@gmail.com>
2021-06-10 23:32:33 -0400

    [add] apply app owner by default when creating new app

--- api/app/controllers/app.js
+++ api/app/controllers/app.js
@@ -1,4 +1,5 @@
 'use strict';
+const { default: createStrapi } = require("strapi");
 const {sanitizeEntity} = require("strapi-utils");
 
 /**
@@ -7,9 +8,25 @@ const {sanitizeEntity} = require("strapi-utils");
  */
 
 module.exports = {
-    async findOneUrl(ctx){
-        const {url} = ctx.params;
-        const entity = await strapi.services.app.findOne({url});
+    // async findOneUrl(ctx){
+    //     const {url} = ctx.params;
+    //     const entity = await strapi.services.app.findOne({url});
+    //     return sanitizeEntity(entity, {model: strapi.models.app});
+    // },
+
+    /**
+     * Create a record with authenticated user id by default
+     *
+     * @returns {Object}
+     */
+
+    async create(ctx){
+        let entity;
+        ctx.request.body.owner = ctx.state.user.id;
+        entity = await strapi.services.app.create(ctx.request.body);
+
         return sanitizeEntity(entity, {model: strapi.models.app});
     }
+
+
 };
