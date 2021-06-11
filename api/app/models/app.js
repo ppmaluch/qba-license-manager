836117a99530e080623a8cbcb431270b0c1b03ac
ppmaluch <ppmaluch@gmail.com>
2021-06-11 00:30:19 -0400

    [add] slug system

--- api/app/models/app.js
+++ api/app/models/app.js
@@ -1,8 +1,31 @@
 'use strict';
+const slugify = require('slugify');
+const replaceString = require('replace-string');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+
+    /**
+     * Trigger before data creation
+     */
+
+    lifecycles: {
+        async beforeCreate(data){
+            if (data.app_id){
+                let cleanAppId = replaceString(data.app_id, '.',' ');
+                data.url = slugify(cleanAppId, {lower:true});
+            }
+        },
+
+        async beforeUpdate(params, data){
+            if (data.app_id){
+                let cleanAppId = replaceString(data.app_id, '.',' ');
+                data.url = slugify(cleanAppId, {lower:true});
+            }
+        },
+    }
+};
